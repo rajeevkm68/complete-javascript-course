7316f35235c15da0394de39acbee416ed1be8f06
rajeevkm68 <rajeevkm68@gmail.com>
2026-05-01 10:33:18 +0530

    Complete

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,105 @@
 'use strict';
+
+/*
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    let output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      // Creating NEW variable with same name as outer scope's variable
+      const firstName = 'Steven';
+
+      // Reasssigning outer scope's variable
+      output = 'NEW OUTPUT!';
+
+      const str = `Oh, and you're a millenial, ${firstName}`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    // console.log(str);
+    console.log(millenial);
+    // console.log(add(2, 3));
+    console.log(output);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Jonas';
+calcAge(1991);
+// console.log(age);
+// printAge();
+
+
+const calcAge = {
+  name: 'Rajeev',
+  greet: () => {
+    console.log(`Hey ${calcAge.name}`);
+  },
+  myArray: [1, 2, 3],
+  myObject: {
+    name: 'Rajeev',
+    greet: function () {
+      console.log(`Hey ${this.name}`);
+    },
+  },
+};
+
+calcAge.myObject.greet();
+console.log(calcAge.myArray[0]);
+
+
+function greet() {
+  console.log(`Hey ${this.name}`);
+}
+
+const user = {
+  name: 'Rajeev',
+};
+
+greet.call(user);
+
+greet.apply(user);
+
+const greetUser = greet.bind(user);
+greetUser();
+
+
+
+const original = {
+  name: 'Rajeev',
+  address: {
+    city: 'Bangalore',
+  },
+};
+
+const shallowCopy = { ...original };
+
+shallowCopy.address.city = 'Mumbai';
+
+console.log('Original:', original.address.city); // ❗ "Mumbai"
+console.log('Shallow Copy:', shallowCopy.address.city); // ❗ "Mumbai"
+
+*/
+
+const original = {
+  name: 'Rajeev',
+  address: {
+    city: 'Bangalore',
+  },
+};
+
+const deepCopy = structuredClone(original);
+
+deepCopy.address.city = 'Mumbai';
+
+console.log('Original:', original.address.city); // ✅ "Bangalore"
+console.log('Deep Copy:', deepCopy.address.city); // ✅ "Mumbai"
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 // Data needed for a later exercise
 const flights =
   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
@@ -21,6 +21,8 @@ const mexicanFoods = new Set([
   'avocado',
   'garlic',
 ]);
+
+*/
 
 // Data needed for first part of the section
 const restaurant = {
@@ -44,4 +46,43 @@ const restaurant = {
       close: 24,
     },
   },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  starterIndex: 2,
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+});
+
+const { categories, openingHours, name } = restaurant;
+console.log(
+  `The restaurant ${name} has ${categories.length} categories and opens at ${openingHours.thu.open} on Thursdays.`,
+);
+
+const [category1, , category3] = categories;
+console.log(category1, category3);
+
+const {
+  thu: { open: thuOpen, close: thuClose } = {},
+  fri: { open: friOpen, close: friClose } = {},
+  sat: { open: satOpen, close: satClose } = {},
+} = openingHours;
+console.log(thuOpen, thuClose, friOpen, friClose, satOpen, satClose);
+
+const newRestaurant = { ...restaurant, founder: 'Guiseppe', foundedIn: 1998 };
+newRestaurant.name = 'Ristorante Roma';
+newRestaurant.categories.push('New Category');
+console.log(newRestaurant);
+console.log(restaurant);
