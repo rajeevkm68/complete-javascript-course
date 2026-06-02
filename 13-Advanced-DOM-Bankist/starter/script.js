be5c70afd577f1e81f4708c411ad0b5fa53c84ed
rajeevkm68 <rajeevkm68@gmail.com>
2026-06-02 18:26:13 +0530

    New Commit

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,10 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const header = document.querySelector('header');
+
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +21,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +31,36 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+const allSections = document.querySelectorAll('.section');
+
+const allButtons = document.getElementsByTagName('button');
+
+const allBtn = document.getElementsByClassName('btn');
+
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+message.innerHTML =
+  'We use cookies for improved security and analytics <button class="btn btn--close-cookie">Got it!</button>';
+
+header.prepend(message);
+header.append(message);
+
+document
+  .querySelector('.btn')
+  .addEventListener(
+    'click',
+    e => (document.querySelector('.btn').style.backgroundColor = 'red'),
+  );
+
+message.style.backgroundColor = 'rgb(130,230,230)';
+message.style.color = 'white';
+message.style.width = '120%';
+
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height, 10) + 10 + 'px';
+
+const logo = document.querySelector('.nav__logo');
+
+console.log(logo);
+logo.classList.add('c', 'j');
